6698702043f56b3084c64b56c152ba596afd405b
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-30 16:23:16 -0300

    Finish working service

--- service/service.go
+++ service/service.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -29,42 +31,49 @@ func (s *UserService) getDb() (gorm.DB, error) {
 	if err != nil {
 		return db, errors.New("Error opening DB")
 	}
+	db.DropTableIfExists(User{})
+	db.CreateTable(User{})
 	return db, err
 }
 
 func (s *UserService) Run() {
-	//db, err := s.getDb()
-	//if err != nil {
-	//log.Println("Error getting database")
-	//return
-	//}
+	db, err := s.getDb()
+	if err != nil {
+		log.Println("Error getting database")
+		return
+	}
 
-	//resource := &UserResource{db: db}
+	resource := &UserResource{db: db}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/users", GetUsersHandler).Methods("GET")
-	r.HandleFunc("/users", CreateUserHandler).Methods("POST")
-	r.HandleFunc("/users/{id}", GetUserHandler).Methods("GET")
-	r.HandleFunc("/users/{id}", DeleteUserHandler).Methods("DELETE")
-	r.HandleFunc("/users/{id}", UpdateUserHandler).Methods("PUT")
+	r.HandleFunc("/users", resource.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users", resource.CreateUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", resource.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", resource.DeleteUserHandler).Methods("DELETE")
+	r.HandleFunc("/users/{id}", resource.UpdateUserHandler).Methods("PUT")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", r)
 }
 
-func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+func (ur *UserResource) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		fmt.Errorf("Error decoding JSON")
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
 	}
 
+	ur.db.Create(&user)
 	user_href := fmt.Sprintf("/users/%d", user.Id)
 	w.Header().Set("Location", user_href)
 	w.WriteHeader(http.StatusCreated)
 }
 
-func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	user := User{1, "Lucas", 29}
+func (ur *UserResource) GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	ur.db.Where(&User{Id: id}).First(&user)
 
 	data, err := json.Marshal(user)
 	if err != nil {
@@ -73,9 +82,11 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
-func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	users := []User{User{1, "Lucas", 29}, User{2, "Majoe", 30}}
+func (ur *UserResource) GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	//users := []User{User{1, "Lucas", 29}, User{2, "Majoe", 30}}
+	var users []User
 
+	ur.db.Find(&users)
 	data, err := json.Marshal(users)
 	if err != nil {
 		fmt.Errorf("Error encoding JSON")
@@ -83,17 +94,31 @@ func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
-func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
-	//params := mux.Vars(r)
-	//id := params["id"]
-	//TODO
-	//delete from database
+func (ur *UserResource) DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	ur.db.Where(&User{Id: id}).First(&user)
+	ur.db.Delete(&user)
+
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
-	//params := mux.Vars(r)
-	//id := params["id"]
+func (ur *UserResource) UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user, newUser User
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	ur.db.Where(&User{Id: id}).First(&user)
+	ur.db.Model(&user).Updates(newUser)
 
 	w.WriteHeader(http.StatusNoContent)
 }
